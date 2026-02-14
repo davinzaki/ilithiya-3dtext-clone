1a858254d2a94bec3d43e9385fcb2a6e0f9f8dad
Davin Zaki Pradipa <davinzaki12@gmail.com>
2026-02-15 01:06:11 +0700

    fix: async load failed build

--- src/script.js
+++ src/script.js
@@ -10,9 +10,11 @@ import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 // Debug
 // const gui = new GUI();
 
-// Font
-const fontLoader = new FontLoader();
-const font = await fontLoader.loadAsync("/fonts/Poppins_Regular.json");
+// Watermark
+const watermark = document.createElement("div");
+watermark.className = "watermark";
+watermark.innerText = "made by @davinzaki";
+document.body.appendChild(watermark);
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -20,127 +22,135 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// Font
+const textGroup = new THREE.Group();
+scene.add(textGroup);
+
+const fontLoader = new FontLoader();
+fontLoader.load("/fonts/Poppins_Regular.json", (font) => {
+  const textGeometry = new TextGeometry("aspiring", {
+    font: font,
+    size: 1,
+    depth: 0.8,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.05,
+    bevelSize: 0.06,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  const text = new THREE.Mesh(textGeometry, material);
+
+  const textGeometry2 = new TextGeometry("creative", {
+    font: font,
+    size: 1,
+    depth: 0.8,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.05,
+    bevelSize: 0.06,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  const text2 = new THREE.Mesh(textGeometry2, material);
+
+  const textGeometry3 = new TextGeometry("developer", {
+    font: font,
+    size: 1,
+    depth: 0.8,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.05,
+    bevelSize: 0.06,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  const text3 = new THREE.Mesh(textGeometry3, material);
+
+  scene.add(text);
+
+  text.position.y = 1.5;
+  text2.position.y = 0;
+  text3.position.y = -1.5;
+
+  textGroup.add(text, text2, text3);
+
+  const box = new THREE.Box3().setFromObject(textGroup);
+
+  const center = new THREE.Vector3();
+  box.getCenter(center);
+
+  textGroup.children.forEach((child) => {
+    child.position.x -= center.x;
+    child.position.y -= center.y;
+    child.position.z -= center.z;
+  });
+
+  textGroup.position.set(0, 0, -6);
+});
+
 /**
  * Textures
  */
+
+const randomNumber = Math.floor(Math.random() * (11 - 1)) + 1;
 const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load(`./textures/matcaps/9.jpeg`);
+matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
  * Object
  */
-// const cube = new THREE.Mesh(
-//   new THREE.BoxGeometry(1, 1, 1),
-//   new THREE.MeshBasicMaterial(),
-// );
 
-const group = new THREE.Group();
-scene.add(group);
+const variantGroup = new THREE.Group();
+scene.add(variantGroup);
 
-const largeGroup = new THREE.Group();
-scene.add(largeGroup);
+// const material = new THREE.MeshNormalMaterial();
+// material.flatShading = true;
 
-const material = new THREE.MeshNormalMaterial();
-material.flatShading = true;
+const material = new THREE.MeshMatcapMaterial();
+material.matcap = matcapTexture;
+
 // textGeometry.translate(
 //   -(textGeometry.boundingBox.max.x - 0.02) * 0.5, // Subtract bevel size
 //   -(textGeometry.boundingBox.max.y - 0.02) * 0.5, // Subtract bevel size
 //   -(textGeometry.boundingBox.max.z - 0.03) * 0.5, // Subtract bevel thickness
 // );
 
-const textGeometry = new TextGeometry("aspiring", {
-  font: font,
-  size: 1,
-  depth: 0.8,
-  curveSegments: 12,
-  bevelEnabled: true,
-  bevelThickness: 0.05,
-  bevelSize: 0.06,
-  bevelOffset: 0,
-  bevelSegments: 5,
-});
-const text = new THREE.Mesh(textGeometry, material);
-
-const textGeometry2 = new TextGeometry("creative", {
-  font: font,
-  size: 1,
-  depth: 0.8,
-  curveSegments: 12,
-  bevelEnabled: true,
-  bevelThickness: 0.05,
-  bevelSize: 0.06,
-  bevelOffset: 0,
-  bevelSegments: 5,
-});
-const text2 = new THREE.Mesh(textGeometry2, material);
-
-const textGeometry3 = new TextGeometry("developer", {
-  font: font,
-  size: 1,
-  depth: 0.8,
-  curveSegments: 12,
-  bevelEnabled: true,
-  bevelThickness: 0.05,
-  bevelSize: 0.06,
-  bevelOffset: 0,
-  bevelSegments: 5,
-});
-const text3 = new THREE.Mesh(textGeometry3, material);
-
-scene.add(text)
-
-text.position.y = 1.5;
-text2.position.y = 0;
-text3.position.y = -1.5;
-
-group.add(text, text2, text3);
-
-const box = new THREE.Box3().setFromObject(group);
-
-const center = new THREE.Vector3();
-box.getCenter(center);
-
-group.children.forEach((child) => {
-  child.position.x -= center.x;
-  child.position.y -= center.y;
-  child.position.z -= center.z;
-});
-
-group.position.set(0, 0, 0);
-
 const scale = Math.random();
 
-for (let i = 0; i < 80; i++) {
+for (let i = 0; i < 100; i++) {
   const torusGeometry = new THREE.TorusGeometry(1, 0.5, 28, 45);
 
   const torus = new THREE.Mesh(torusGeometry, material);
 
-  torus.position.x = (Math.random() - 0.5) * 50;
-  torus.position.y = (Math.random() - 0.5) * 50;
-  torus.position.z = (Math.random() - 0.5) * 50;
+  torus.position.x = (Math.random() - 0.5) * 80;
+  torus.position.y = (Math.random() - 0.5) * 80;
+  torus.position.z = (Math.random() - 0.5) * 80;
 
   torus.rotation.x = Math.random() * Math.PI;
   torus.rotation.y = Math.random() * Math.PI;
 
   scene.add(torus);
 
-  largeGroup.add(torus);
+  variantGroup.add(torus);
 }
 
-for (let i = 0; i < 80; i++) {
+for (let i = 0; i < 100; i++) {
   const boxGeometry = new THREE.BoxGeometry(1);
 
-  const box = new THREE.Mesh(boxGeometry, material);
+  const boxMesh = new THREE.Mesh(boxGeometry, material);
 
-  box.position.x = (Math.random() - 0.5) * 50;
-  box.position.y = (Math.random() - 0.5) * 50;
-  box.position.z = (Math.random() - 0.5) * 50;
+  boxMesh.position.x = (Math.random() - 0.5) * 80;
+  boxMesh.position.y = (Math.random() - 0.5) * 80;
+  boxMesh.position.z = (Math.random() - 0.5) * 80;
 
-  box.rotation.x = Math.random() * Math.PI;
-  box.rotation.y = Math.random() * Math.PI;
+  boxMesh.rotation.x = Math.random() * Math.PI;
+  boxMesh.rotation.y = Math.random() * Math.PI;
 
-  scene.add(box);
+  scene.add(boxMesh);
 
-  largeGroup.add(box);
+  variantGroup.add(boxMesh);
 }
 
 /**
@@ -175,7 +185,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100,
 );
-camera.position.z = 8;
+camera.position.z = 20;
+camera.lookAt(textGroup.position);
 scene.add(camera);
 
 // Controls
@@ -205,20 +216,30 @@ const smoothCursor = {
   x: 0,
   y: 0,
 };
+let isMoving = false;
+let idleTimer;
 
 window.addEventListener("mousemove", (event) => {
   cursor.x = event.clientX / sizes.width - 0.5;
   //   cursor.y = -(event.clientY / sizes.width - 0.5);
   cursor.y = event.clientY / sizes.height - 0.5;
+
+  isMoving = true;
+
+  clearTimeout(idleTimer);
+  idleTimer = setTimeout(() => {
+    isMoving = false;
+  }, 500);
 });
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  // group.rotation.z = Math.sin();
+  textGroup.rotation.z = Math.sin(elapsedTime * 0.3);
+  variantGroup.rotation.z = -Math.sin(elapsedTime * 0.3);
 
-  smoothCursor.x += (cursor.x - smoothCursor.x) * 0.015;
-  smoothCursor.y += (cursor.y - smoothCursor.y) * 0.015;
+  smoothCursor.x += (cursor.x - smoothCursor.x) * 0.05;
+  smoothCursor.y += (cursor.y - smoothCursor.y) * 0.05;
 
   // Hitung jarak kursor dari titik tengah (0,0)
   // Math.sqrt(x*x + y*y)
@@ -228,19 +249,23 @@ const tick = () => {
 
   // Nilai 6 adalah jarak terdekat (saat di tengah)
   // Nilai 15 adalah tambahan jarak saat di pojok layar
-  const baseZoom = 10;
-  const zoomIntensity = 10;
+  const baseZoom = 6;
+  const zoomIntensity = 15;
 
   camera.position.z = baseZoom + distanceFromCenter * zoomIntensity;
 
-  // Perbaikan rotasi dan posisi X, Y agar tetap smooth
-  group.rotation.x = smoothCursor.y * 0.8;
-  group.rotation.y = smoothCursor.x * 0.8;
-  largeGroup.rotation.x = smoothCursor.y * 0.8;
-  largeGroup.rotation.y = smoothCursor.x * 0.8;
+  // rotasi dan posisi X, Y agar smooth
+  textGroup.rotation.x = smoothCursor.y * 0.8;
+  textGroup.rotation.y = smoothCursor.x * 0.8;
+  variantGroup.rotation.x = smoothCursor.y * 0.8;
+  variantGroup.rotation.y = smoothCursor.x * 0.8;
 
   camera.position.x = smoothCursor.x * 5;
-  camera.position.y = -smoothCursor.y * 5; 
+  camera.position.y = -smoothCursor.y * 5;
+
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  // camera.position.y = cursor.y * 2;
 
   // camera.lookAt(new THREE.Vector3(0, 0, 0));
 
